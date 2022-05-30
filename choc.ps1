272cc35f4067290b1ecd7d650ae7c356b8a1feae
DP-Miller <99252775+DP-Miller@users.noreply.github.com>
2022-05-30 20:59:05 +0100

    Update choc.ps1

--- choc.ps1
+++ choc.ps1
@@ -1,5 +1,7 @@
 write-host 'Choco install'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+write-host choco feature enable -n allowGlobalConfirmation
 write-host 'Choco complete'
 write-host 'install adobe'
 choco install adobereader
