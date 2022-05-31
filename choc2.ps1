d1a4522d29b295f9b1de414568ee0476aa6dbe81
DP-Miller <99252775+DP-Miller@users.noreply.github.com>
2022-05-31 15:28:42 +0100

    Update choc2.ps1

--- choc2.ps1
+++ choc2.ps1
@@ -1,4 +1,5 @@
 write-host 'Choco install'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
-write-host choco feature enable -n allowGlobalConfirmation
+choco install notepadplusplus
+write-host choco install npp
